4961b37d131e86499e1f0a4d311410362227897c
Arjit1512 <avadhanamarjit15@gmail.com>
2024-09-29 16:43:37 +0530

    edit

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
-
+console.log(process.env.FIREBASE_SERVICE_ACCOUNT_PATH)
 //initialize FireBase
 
 const serviceAccount = JSON.parse(fs.readFileSync(path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH)));
